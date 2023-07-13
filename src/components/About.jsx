b8b40c4409e233c5eb39660b94da8bfbbb934367
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-07-13 15:05:29 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,10 +47,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]'
       >
-        Hey there, I am a 3rd Year student, curently pursuing B.Tech. in Computer Science and Engineering 
+        Hey there, I am a Final Year student, curently pursuing B.Tech. in Computer Science and Engineering 
         (CSE) at Jaypee Institute of Information Technology (JIIT).        
         <br></br>
-        An explorer and versatile person who enjoys problem solving and development. 
+        I'm an explorer and versatile person who enjoys problem solving and development. 
         Constantly eager to learn new algorithms, tools & technologies and explore various domains. I like
         contributing to projects where I can gear up my skills and knowledge.
         <br></br>
