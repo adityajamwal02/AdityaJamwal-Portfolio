d62633bd121cc021b974aa1684b4b9fbeb468caf
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2024-10-05 01:28:33 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Aditya</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Developer | Innovator | Leader <br className='sm:block hidden' />
+            Software Engineer | Developer | Innovator <br className='sm:block hidden' />
           </p>
         </div>
       </div>
